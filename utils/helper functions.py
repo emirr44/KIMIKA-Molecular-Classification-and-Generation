70944e518b9cd381ea0b1d5b4aecd11af5d20056
Emir Baručija <emirbarrucija0@gmail.com>
2025-06-09 12:12:37 +0200

    old files
    old files

--- utils/helper functions.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def cloneRepository(repositoryURL, destination):
-    try:
-        subprocess.run(["git", "clone", repositoryURL, destination], check=True)
-        print(f"Repository cloned to {destination}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error cloning repository: {e}")
-
